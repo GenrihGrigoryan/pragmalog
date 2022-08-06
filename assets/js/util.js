3155ef1420e0adbec7e8fc0e33d625c17ecb5a11
Genrih Grigoryan <grigoryang1995@yandex.ru>
2022-08-07 00:14:14 +0300

    upd

--- assets/js/util.js
+++ assets/js/util.js
@@ -185,8 +185,8 @@ const displayResults = (finalResults, extractHighlight = false) => {
   const results = document.getElementById("results-container")
   if (finalResults.length === 0) {
     results.innerHTML = `<button class="result-card">
-                    <h3>No results.</h3>
-                    <p>Try another search term?</p>
+                    <h3>Нет результатов.</h3>
+                    <p>Попробуйте другой поисковой запрос.</p>
                 </button>`
   } else {
     results.innerHTML = finalResults
